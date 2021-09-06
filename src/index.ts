d6a7ce6081e3945b6b3353baab0ec8f4f450ebab
amontalvof <amontalvof92@gmail.com>
2021-09-06 08:53:47 -0500

    feat: Subscription and unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,26 @@ const observer: Observer<any> = {
     error: (error) => console.warn('error:', error),
     complete: () => console.info('completed'),
 };
+
+const interval$ = new Observable<number>((subscriber) => {
+    let counter = 0;
+    const interval = setInterval(() => {
+        counter += 1;
+        subscriber.next(counter);
+    }, 1000);
+    return () => {
+        clearInterval(interval);
+        console.log('interval destroyed');
+    };
+});
+
+const subscription1 = interval$.subscribe((num) => console.log('Num:', num));
+const subscription2 = interval$.subscribe((num) => console.log('Num:', num));
+const subscription3 = interval$.subscribe((num) => console.log('Num:', num));
+
+setTimeout(() => {
+    subscription1.unsubscribe();
+    subscription2.unsubscribe();
+    subscription3.unsubscribe();
+    console.log('timeout completed');
+}, 3000);
