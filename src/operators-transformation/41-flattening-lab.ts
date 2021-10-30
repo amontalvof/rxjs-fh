140b56abf3fe278143e0f599eabe4872597eb55e
amontalvof <amontalvof92@gmail.com>
2021-10-30 11:50:09 -0500

    feat: flattening lab

---
+++ src/operators-transformation/41-flattening-lab.ts
@@ -0,0 +1,61 @@
+// ! 84 - Comparison exercise between the mergeMap, switchMap and exhaustMap
+
+import { of, fromEvent, Observer } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import {
+    tap,
+    map,
+    pluck,
+    catchError,
+    mergeMap,
+    switchMap,
+    exhaustMap,
+} from 'rxjs/operators';
+
+const observer: Observer<any> = {
+    next: (value) => console.log('next:', value),
+    error: (error) => console.warn('error:', error),
+    complete: () => console.info('completed'),
+};
+
+// * Creating a form
+const form = document.createElement('form');
+const inputEmail = document.createElement('input');
+const inputPassword = document.createElement('input');
+const submitButton = document.createElement('button');
+
+// * Configurations
+inputEmail.type = 'email';
+inputEmail.placeholder = 'Email';
+inputEmail.value = 'eve.holt@reqres.in';
+
+inputPassword.type = 'password';
+inputPassword.placeholder = 'Password';
+inputPassword.value = 'cityslicka';
+
+submitButton.innerHTML = 'Submit';
+
+form.append(inputEmail, inputPassword, submitButton);
+document.querySelector('body').append(form);
+
+//* Helper
+const requestHttpLogin = (userPass) => {
+    return ajax.post('https://reqres.in/api/login?delay=1', userPass).pipe(
+        pluck('response', 'token'),
+        catchError(() => of('xxx'))
+    );
+};
+
+// * Streams
+const submitForm$ = fromEvent<SubmitEvent>(form, 'submit').pipe(
+    tap((event) => event.preventDefault()),
+    map((event) => ({
+        email: event.target[0].value,
+        password: event.target[1].value,
+    })),
+    // mergeMap(requestHttpLogin)
+    // switchMap(requestHttpLogin)
+    exhaustMap(requestHttpLogin)
+);
+
+submitForm$.subscribe(observer);
